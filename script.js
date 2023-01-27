5a2f94f75189a78234fa84395e86dfd425238cfb
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-27 17:55:53 +0200

    draft function added to create recent cities

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+var recentCities = [];
+
 $("#search-button").on("click", function(event) {
     event.preventDefault();
     var city = $("#search-input").val();
+    recentCities.push(city);
     console.log(city);
+    recentSearches();
     $("#search-input").val('');
 })
+
+function recentSearches() {
+    console.log(recentCities)
+}
