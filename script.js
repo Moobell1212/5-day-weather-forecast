27b6c941b6123486eb4daab4d37d0fb42222b27b
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-28 21:25:15 +0200

    function to add current weather added

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let recentCities = [];
-console.log(recentCities)
+// console.log(recentCities)
 
 $("#search-button").on("click", function (event) {
     event.preventDefault();
     // cityinput value
     var city = $("#search-input").val();
-
     // push cityinput to the array
     recentCities.push(city);
-
     // add new recentCities array to local storage
     localStorage.setItem("pastCities", JSON.stringify(recentCities));
-
     // clear the searchbar
     $("#search-input").val('');
     $("#history").empty();
@@ -20,15 +17,18 @@ $("#search-button").on("click", function (event) {
 
 // function for adding past searched cities
 function createPastChoices() {
-    var history = $("#history");
-    var recentSearch = $('<h4>').text("Recent searches:").css('margin-top', 10);
-    history.append(recentSearch);
+    // add the title "Recent searches" under the search section
+    $("#history").append($('<h4>').text("Recent searches:").css('margin-top', 10));
+    // gets the array of past searched cities from local storage
     var pastCities = JSON.parse(localStorage.getItem("pastCities"));
-    console.log(pastCities)
-
-    // buttons added to page for ever past city searched
+    // console.log(pastCities);
+    // buttons added to page for every past city searched
     for (let i = 0; i < pastCities.length; i++) {
-        var cityButton = $('<button>').text(pastCities[i]).css('width', 290).css('margin-top', 5)
-        history.append(cityButton)
+        var cityButton = $('<button>').text(pastCities[i]).css('width', 290).css('margin-top', 5).css('border-radius', 7);
+        $("#history").append(cityButton)
     }
+}
+
+function getWeather() {
+
 }
