4d433e11c9dbebef00c58ecc63f257c75e1d7f78
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-28 20:09:49 +0200

    new search now does not add another 'Recent cities'

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-var recentCities = [];
+let recentCities = [];
+console.log(recentCities)
 
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
     event.preventDefault();
+    // cityinput value
     var city = $("#search-input").val();
+
+    // push cityinput to the array
     recentCities.push(city);
-    console.log(city);
-    recentSearches();
+
+    // add new recentCities array to local storage
+    localStorage.setItem("Past cities", recentCities);
+
+    // clear the searchbar
     $("#search-input").val('');
+
+    $("#history").empty();
+
+    createPastChoices()
 })
 
-function recentSearches() {
-    console.log(recentCities)
+function createPastChoices() {
+    var history = $("#history");
+    var recentSearch = $('<h4>').text("Recent searches:");
+    history.append(recentSearch);
+    localStorage.names = JSON.stringify(names);
+    var storedNames = JSON.parse(localStorage.names);
+    console.log(storedNames)
 }
+
+//  // loop to make buttons for recently searched cities
+//  for (let i=0 ; i<recentCities.length; i++) {
+//     localStorage.setItem()
+//     var cityButton = $('<button>').text(recentCities[i]);
+//     history.append(cityButton)
+//     }
