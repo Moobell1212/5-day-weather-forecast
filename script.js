66feb5820a106e7d8ac40f8c70d7b6bdf52c1223
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-30 21:42:51 +0200

    color style added

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function createPastChoices() {
     var pastCities = JSON.parse(localStorage.getItem("pastCities"));
     // buttons added to page for every past city searched
     for (let i = 0; i < pastCities.length; i++) {
-        var cityButton = $('<button>').text(pastCities[i]).css('width', '15em').css({'margin-top': 5, 'border-radius': 7, 'height': 40});
+        var cityButton = $('<button>').text(pastCities[i]).addClass("btn-info").css({'width': '100%', 'margin-top': 5, 'border-radius': 7, 'height': 40});
         $("#history").append(cityButton)
     }
 }
@@ -45,7 +45,7 @@ function getWeather() {
             var date = moment().format("DD/MM/YYYY");
             // get the current weather icon
             var iconID = response.list[0].weather[0].icon;
-            var icon = $('<img>').attr("src", "https://openweathermap.org/img/wn/" + iconID + ".png");
+            var icon = $('<img>').attr("src", "https://openweathermap.org/img/wn/" + iconID + ".png").css({"height": 60, "width": 60});
             // add city name and date
             var cityH1 = $('<h1>').text(city + " (" + date + ") ");
             $('#today').append(cityH1.append(icon));
@@ -73,22 +73,21 @@ function getWeather() {
                 // forecast card dates
                 var dates = moment.unix(forecast.dt).format("DD/MM/YYYY");
                 // add forecast card
-                var card = $('<div>').addClass("card mb-3 p-2");
+                var card = $('<div>').addClass("card mb-3 p-2 text-white bg-info");
                 // card dates 
                 var cardDates = $('<h4>').addClass("card-title").text(dates);
                 // forecast icons 
                 var cardIconID = forecast.weather[0].icon;
                 var icon = $('<img>').attr("src", "https://openweathermap.org/img/wn/" + cardIconID + ".png").css({"height": 50, "width": 50});
                 // forecast temperature
-                var cardTemp = $('<h5>').text("Temperature: " + (forecast.main.temp - 273.15).toFixed() + "°C");
+                var cardTemp = $('<h6>').text("Temperature: " + (forecast.main.temp - 273.15).toFixed() + "°C");
+                // forecast wind speed
+                var cardWind = $('<h6>').text("Wind Speed: " + forecast.wind.speed + " KPH")
 
-
-
-                $("#forecast").append(card.append(cardDates, icon, cardTemp));
+                $("#forecast").append(card.append(cardDates, icon, cardTemp, cardWind));
             })
 
-            // The temperature
-            // Wind speed
+            
             // The humidity
         })
 }
